4f9021c8d3a0052bc3e2fd1deb67f906d8f48ac0
Adele <adeler.ang@gmail.com>
2021-06-24 19:38:56 +0300

    Adding login page object

---
+++ pages/LoginPage.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from config.locators import TestLocators
+
+
+class LoginPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def set_username(self, username):
+        self.driver.find_element_by_id(TestLocators.locator_username_id).clear()
+        self.driver.find_element_by_id(TestLocators.locator_username_id).send_keys(username)
+
+    def set_password(self, password):
+        self.driver.find_element_by_id(TestLocators.locator_password_id).clear()
+        self.driver.find_element_by_id(TestLocators.locator_password_id).send_keys(password)
+
+    def click_login(self):
+        self.driver.find_element_by_xpath(TestLocators.locator_submit_xpath).click()
