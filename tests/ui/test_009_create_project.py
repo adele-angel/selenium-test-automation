ada2ed5ffbb45e150c06936614701c54a562a366
Adele <adeler.ang@gmail.com>
2021-06-30 17:54:18 +0300

    Adding general UI tests

--- tests/ui/test_009_create_project.py
+++ tests/ui/test_009_create_project.py
@@ -1,2 +1,51 @@
-def test_009_create_project():
-    pass
+"""
+Test 009 - UI - Create Project
+
+Steps:
+    1. Login to OpenProject
+    2. On "Home" page, click "+ Project" green button
+    3. On the "New project" page, type a unique value for the project name. The name should contains letters (upper & lower case), numbers, spaces and some special characters (,/#@$%)
+    4. Click "ADVANCED SETTINGS" title
+    5. Type some text to the description text box
+    6. Verify the value of the "Identifier" field
+    7. Select status "On track"
+    8. Click "Create"
+    9. On "Work packages" page, top left corner: verify the text on the button
+"""
+
+
+from config.credentials import Credentials
+from framework.pages.HomePage import HomePage
+from framework.pages.NewProjectPage import NewProjectPage
+from infra.shared_steps import SharedSteps
+
+
+def test_009_create_project(setup):
+    driver = setup
+    driver.get(Credentials.BASE_URL)
+
+    # Login to OpenProject
+    SharedSteps.login_steps(driver)
+
+    # On "Home" page
+    home_page = HomePage(driver)
+    # Click "+ Project" green button
+    home_page.click_new_project()
+
+    # On the "New project" page
+    new_project_page = NewProjectPage(driver)
+    # type a unique value for the project name
+    new_project_page.set_project_name(Credentials.NEW_PROJECT_NAME)
+    # Click "ADVANCED SETTINGS" title
+    new_project_page.click_advanced_settings()
+    # Type some text to the description text box
+    new_project_page.set_project_description(Credentials.NEW_PROJECT_DESCRIPTION)
+    # Select status "On track"
+    new_project_page.set_status(Credentials.NEW_PROJECT_STATUS)
+
+    # Click "Create" and save the project
+    new_project_page.save_new_project()
+
+    # TODO: Do ASSERT On "Work packages" page, top left corner: verify the text on the button
+    # TODO: Do ASSERT - Button text matches the project name entered in step 3
+
--- tests/ui/test_010_create_task.py
+++ tests/ui/test_010_create_task.py
@@ -1,2 +1,53 @@
-def test_009_create_task():
-    pass
+"""
+Test 010 - UI - Create Task
+
+Prerequisites:
+    A project named "TestProject1" already exists
+
+Steps:
+    1. Login to OpenProject
+    2. On "Home" page, top-left corner, click "Select a project" menu button, and select "TestProject1" from the drop-down
+    3. On the "Project Overview" page, left side menu, click "Work packages". Once on the "Work packages" page, note the number of rows displayed in the work packages table.
+    4. Click "+ Create" green button and select "TASK"
+    5. Verify the text "New TASK" on top of the form that got opened on the right side
+    6. Type unique strings into the subject and description boxes
+    7. Click "Save" button
+    8. Verify that a new row was added to the work packages table
+    9. Verify the subject and type of the last table row
+"""
+
+from config.credentials import Credentials
+from framework.pages.WorkPackagesPage import WorkPackagesPage
+from infra.shared_steps import SharedSteps
+
+
+def test_009_create_task(setup):
+    driver = setup
+    driver.get(Credentials.BASE_URL)
+
+    # Login to OpenProject
+    SharedSteps.login_steps(driver)
+
+    # On "Home" page click "Select a project" menu button, and select "TestProject1" from the drop-down
+    SharedSteps.select_project_steps(driver)
+
+    # On the "Project Overview" page, left side menu, click "Work packages"
+    SharedSteps.goto_work_packages_steps(driver)
+
+    # Note the number of rows displayed in the work packages table
+    work_packages_page = WorkPackagesPage(driver)
+    initial_row_count = work_packages_page.get_table_row_count()
+
+    # Click "+ Create" green button and select "TASK"
+
+    # Verify the text "New TASK" on top of the form that got opened on the right side
+    # TODO: ASSERT The title of the form is "New TASK"
+
+    # Type unique strings into the subject and description boxes
+    # Click "Save" button
+
+    # Verify that a new row was added to the work packages table
+    # TODO: ASSERT
+
+    # Verify the subject and type of the last table row
+    # TODO: ASSERT
