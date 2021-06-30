2d358243273c1e5600155f5c8cda6abdccc1cee8
Adele <adeler.ang@gmail.com>
2021-06-30 09:58:11 +0300

    WorkPackages Api refactor (using HTTP Client)

--- config/api.py
+++ config/api.py
@@ -1,6 +1,6 @@
 class TestAPI:
     BASE_URL = "http://localhost:8080"
-    API_KEY = "9636e757ee9d9e6ca2073dfe69741eca7c900d591e4e343275ec197168587797"
+    API_KEY = "1552e088259db32ed06920ff938baeef913572bfa8b17728697808d4e9d0d51a"
 
     PROJECT_ID = "1"
     PROJECT_TITLE = "Demo project"
--- framework/api/work_packages_api.py
+++ framework/api/work_packages_api.py
@@ -1,38 +1,37 @@
-from config.api import TestAPI
+from requests import Response
+from requests.auth import HTTPBasicAuth
 from infra.http_client import HttpClient, HttpCommand
 
 
 class WorkPackagesApi:
-    def __init__(self, base_url: str):
-        self.http_client = HttpClient(base_url)
+    def __init__(self, base_url: str, api_key: str):
+        self.base_route = '/api/v3/work_packages'
+        self.http_client = HttpClient(base_url).set_auth(HTTPBasicAuth('apikey', api_key))
 
-    # @GET /work_packages/:id
-    def get_work_package(self, work_package_id: str):
-        command = HttpCommand().set_route(f'/work_packages/{work_package_id}').set_method('GET')
+    def set_headers(self, headers):
+        self.http_client.add_default_headers(headers)
+        return self
+
+    def set_auth(self, auth):
+        self.http_client.set_auth(auth)
+        return self
+
+    # GET /work_packages/:id
+    def get_work_package(self, work_package_id: str) -> Response:
+        command = HttpCommand().set_route(f'{self.base_route}/{work_package_id}').set_method('GET')
         return self.http_client.send(command)
 
-    # @POST /work_packages/
+    # POST /work_packages/
     def create_work_package(self, body):
-        command = HttpCommand().set_route('/work_packages').set_method('POST').set_body(body)
+        command = HttpCommand().set_body(body).set_route(self.base_route).set_method('POST')
         return self.http_client.send(command)
 
-    # @PATCH /work_packages/:id
-    def update_work_package(self, project_id: str, body):
-        command = {
-            "type": "PATCH",
-            "headers": self.headers,
-            "auth": self.auth,
-            "route": f'/work_packages/{project_id}',
-            "body": body
-        }
-        return self.http_client.get(command)
+    # PATCH /work_packages/:id
+    def update_work_package(self, work_package_id: str, body):
+        command = HttpCommand().set_body(body).set_route(f'{self.base_route}/{work_package_id}').set_method('PATCH')
+        return self.http_client.send(command)
 
-    # @DELETE /work_packages/:id
-    def delete_work_package(self, project_id: str):
-        command = {
-            "type": "DELETE",
-            "headers": self.headers,
-            "auth": self.auth,
-            "route": f'/work_packages/{project_id}'
-        }
-        return self.http_client.get(command)
+    # DELETE /work_packages/:id
+    def delete_work_package(self, work_package_id: str):
+        command = HttpCommand().set_route(f'{self.base_route}/{work_package_id}').set_method('DELETE')
+        return self.http_client.send(command)
