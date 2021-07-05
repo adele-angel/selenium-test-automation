d86c9a083574a70f6f32cb4ec602cd69337b5f64
Adele <adeler.ang@gmail.com>
2021-07-05 18:35:45 +0300

    Refactor testing on different browsers

--- infra/webdriver_factory.py
+++ infra/webdriver_factory.py
@@ -5,10 +5,10 @@ from config.settings import Settings
 class WebDriverFactory:
     @staticmethod
     def create_driver(driver_name):
+        # Initialize ChromeDriver
         if driver_name == "chrome":
             driver = webdriver.Chrome(executable_path=Settings.CHROME_EXECUTABLE_PATH)
-        elif driver_name == "firefox":
+        # Initialize GekoDriver
+        if driver_name == "firefox":
             driver = webdriver.Firefox(executable_path=Settings.FIREFOX_EXECUTABLE_PATH)
-        else:  # Default browser is Chrome
-            driver = webdriver.Chrome(executable_path=Settings.CHROME_EXECUTABLE_PATH)
         return driver
--- tests/ui/conftest.py
+++ tests/ui/conftest.py
@@ -3,38 +3,36 @@ from config.settings import Settings
 from infra.webdriver_factory import WebDriverFactory
 
 
-# TODO: configure browser selection
 @pytest.fixture(scope="function")
-def setup():
-    driver = WebDriverFactory.create_driver("chrome")
-    # if browser.param == "chrome":
-    #     driver = WebDriverFactory.create_driver("chrome")
-    # elif browser.param == "firefox":
-    #     driver = WebDriverFactory.create_driver("firefox")
-    # else:  # Default browser is Chrome
-    #     driver = WebDriverFactory.create_driver("chrome")
+def setup(browser):
+    # Create a driver
+    driver = WebDriverFactory.create_driver(browser)
+    # Return the driver object by the end of the setup
     yield driver
+    # For cleanup, quit the driver instance
     driver.quit()
 
 
 def pytest_addoption(parser):
+    # Setting default browser
     parser.addoption("--browser", action="store", default=Settings.DEFAULT_BROWSER)
+    # Setting default environment
     parser.addoption("--env", action="store", default=Settings.ENV)
 
 
-@pytest.fixture()
+@pytest.fixture
 def browser(request):
     return request.config.getoption("--browser")
 
 
-def pytest_configure(config):  # hook for Adding Environment info to HTML Report
+def pytest_configure(config):  # Hook for Adding Environment info to HTML Report
     config._metadata["Project Name"] = "Selenium Test Automation"
     config._metadata["Environment"] = Settings.ENV
     config._metadata["Tester"] = "Adele Angel"
     config._metadata["Package"] = "python 3.9.5"
 
 
-@pytest.mark.parametrize  # hook for delete/modify environment info to HTML Report
+@pytest.mark.parametrize  # Hook for delete/modify environment info to HTML Report
 def pytest_metadata(metadata):
     metadata.pop("JAVA_HOME", None)
     metadata.pop("Plugins", None)
